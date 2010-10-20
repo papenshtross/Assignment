05573a258b0d99d3d5319c46b844b0524b95fd3f
papenshtross <romanshp@gmail.com>
2010-10-21 00:19:18 +0200

    Project structure and unit tests

---
+++ main/tests.py
@@ -0,0 +1,34 @@
+"""
+This file demonstrates two different styles of tests (one doctest and one
+unittest). These will both pass when you run "manage.py test".
+
+Replace these with more appropriate tests for your application.
+"""
+
+from django.test import TestCase
+from main.models import Profile
+
+class MainTest(TestCase):
+    fixtures = ['initial_data.yaml']
+
+    def test_index(self):
+        response = self.client.get('')
+
+        # Check that the response is 200 OK
+        self.failUnlessEqual(response.status_code, 200)
+        # Check that index.html template is using
+        self.assertTemplateUsed(response, 'index.html', msg_prefix='')
+        # Assign profile objects from test db and acquired from response
+        profile = Profile.objects.get(pk=1)
+        response_profile = response.context['profile']
+        # Check that response contains first_name filed value
+        self.assertContains(response, profile.first_name, count=1
+                            , status_code=200, msg_prefix='')
+        # Check profile fields values
+        self.failUnlessEqual(profile.first_name, response_profile.first_name)
+        self.failUnlessEqual(profile.last_name, response_profile.last_name)
+        self.failUnlessEqual(profile.bio, response_profile.bio)
+        self.failUnlessEqual(profile.cell, response_profile.cell)
+        self.failUnlessEqual(profile.icq, response_profile.icq)
+        self.failUnlessEqual(profile.email, response_profile.email)
+        self.failUnlessEqual(profile.skype, response_profile.skype)
---
+++ urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import *
+from django.contrib import admin
+from main.views import index
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^assignment/', include('assignment.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+)
