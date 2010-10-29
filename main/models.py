1c614f18f1f215e39f1c7b07a5570a816f814a25
papenshtross <romanshp@gmail.com>
2010-10-29 16:18:19 +0200

    gitignore file added, code altered in compliance with pep8 test results, settings.TIME_ZODE added to index page

--- main/models.py
+++ main/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class Profile(models.Model):       
+class Profile(models.Model):
     """My profile model"""
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -19,5 +19,6 @@ class Profile(models.Model):
 class Request(models.Model):
     """Http Request model"""
     request = models.TextField()
+
     def __unicode__(self):
         return self.request
