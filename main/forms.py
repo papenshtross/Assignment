1964b7da1a97be20e8add1b89eeffd0f00d70890
papenshtross <romanshp@gmail.com>
2010-10-30 04:35:41 +0200

    Implementation of reverse edit profile form fields order

--- main/forms.py
+++ main/forms.py
@@ -6,6 +6,16 @@ from django.contrib.admin.widgets import AdminDateWidget
 
 class ProfileForm(forms.ModelForm):
     """Represent form for Profile model"""
+
+    def __init__(self, *args, **kw):
+        super(forms.ModelForm, self).__init__(*args, **kw)
+        profile_fields = Profile._meta.fields[1:]
+        profile_fields.reverse()
+        values = []
+        for field in profile_fields:
+            values.append(field.get_attname())
+        self.fields.keyOrder = values
+
     class Meta:
         """Model assigning"""
         model = Profile
