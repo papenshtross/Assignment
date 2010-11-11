56a0806d9c024bb2ede169e66f66f04793295384
papenshtross <romanshp@gmail.com>
2010-11-11 03:03:20 +0100

    Ticket:10 signal processor implementation

--- main/models.py
+++ main/models.py
@@ -1,5 +1,8 @@
 """Defining application models"""
 from django.db import models
+from django.dispatch.dispatcher import receiver
+from django.db.models.signals import post_save
+from django.db.models.signals import post_delete
 
 
 class Profile(models.Model):
@@ -23,3 +26,31 @@ class Request(models.Model):
 
     def __unicode__(self):
         return self.request
+
+
+class TransactionSignal(models.Model):
+    """Signal processor db model"""
+    model = models.TextField()
+    action = models.TextField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.model, self.action)
+
+
+@receiver(post_save)
+def saved(sender, created, **kwargs):
+    """post_save signal handler"""
+    if sender is not TransactionSignal:
+        if created:
+            action = 'create'
+        else:
+            action = 'update'
+        TransactionSignal.objects.create(model=sender.__name__,
+                                         action=action)
+
+
+@receiver(post_delete)
+def deleted(sender, **kwargs):
+    """post_delete signal handler"""
+    TransactionSignal.objects.create(model=sender.__name__,
+                                         action='delete')
