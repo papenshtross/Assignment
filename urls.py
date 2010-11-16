e355d57dc6e7b4a0b8b0b41807d30e24ebaf1025
papenshtross <romanshp@gmail.com>
2010-11-16 16:11:32 +0100

    ticket:11 Ajax form implementation

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from main.views import index
+from main.views import index, contactForm
 from main.views import edit_profile
 
 # Uncomment the next two lines to enable the admin:
@@ -20,4 +20,12 @@ urlpatterns = patterns('',
     (r'^$', index),
     (r'^profile_edit/(\d+)/$', edit_profile),
     (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    #Direct to template: For non-ajax form.
+    (r'afterform/$', 'django.views.generic.simple.direct_to_template',
+     {"template": 'contact/afterform.html',"extra_content":{"error":False} }),
+    #Handle contact form. Using a named url: Note use in template below.
+    #This url handles contact/ and contact/xhr : The 'xhr' is a flag to tell the
+    #view that this is an ajax POST. I can't recall what it stood for :)
+    url(r'contact/(?P<xhr>.*)$', contactForm,
+        name='contactform'),
 )
