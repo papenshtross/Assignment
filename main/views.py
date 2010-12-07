7905df18820653fb5958f55e0354fef2d25a3ca9
papenshtross <romanshp@gmail.com>
2010-12-07 18:42:13 +0100

    Tests fixed

--- main/views.py
+++ main/views.py
@@ -1,14 +1,14 @@
 """File where views are defined"""
-from main.models import Profile
 from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.template.context import RequestContext
-from main.forms import ProfileForm, ContactForm
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.utils import simplejson
+from main.forms import ProfileForm, ContactForm
+from main.models import Profile
 
 
 def index(request):
